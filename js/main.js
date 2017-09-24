a3dd851880e38249b4fd3d5ec6eea524eb39628d
Eugene Angelo V. Paje <eugeneangelopaje@gmail.com>
2017-09-24 16:46:39 +0800

    Landing & side bar menu finished

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  console.log('Main.js reporting for duty! :)');
+  // Initialize collapse button
+  $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      //onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened
+      //onClose: function(el) { /* Do Stuff* / }, //A function to be called when sideNav is closed
+    }
+  );
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+});
